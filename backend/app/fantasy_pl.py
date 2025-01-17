89686dafc0983c8da9dca3d9d34697818baa44fb
Yusuf Khan <ykhan047@uottawa.ca>
2025-01-17 02:09:47 -0500

    Initial working frontend

--- backend/app/fantasy_pl.py
+++ backend/app/fantasy_pl.py
@@ -2,6 +2,11 @@ from team_builder import build_best_team
 from utils import get_player_position, get_team_name
 from fpl_api import fetch_player_data, fetch_player_summary
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/home')
 def main():
     # Fetch all players' data
     players, teams, positions = fetch_player_data()
@@ -11,14 +16,27 @@ def main():
     # Build the best team from the player data
     best_team, budget_left = build_best_team(players)
 
-    print(f"Best Team Built! Remaining Budget: {budget_left/10.0} million")
+    best_team_list = [[0] * 4 for _ in range(15)]
+    i=0
+
+    
+
     for player in best_team:
         # Make sure position is correctly mapped
         position_name = get_player_position(player['element_type'])
         team_name = team_dict.get(player['team'], "Unknown Team")
-        print(f"{player['web_name']:<15} ({position_name:<3}) {player['now_cost']/10.0:>5.1f} million   ({team_name})")
-            
 
+        best_team_list[i][0]=player['web_name']
+        best_team_list[i][1]=position_name
+        best_team_list[i][2]=player['now_cost']/10.0
+        best_team_list[i][3]=team_name
+        i+=1
+
+    return {
+    "best_team": best_team_list
+}
+
+            
 # Call the main function to run the app
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
